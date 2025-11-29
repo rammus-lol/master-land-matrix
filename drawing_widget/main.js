595b79a1b0520e1cf5d8a10b1b9db5af416934d0
Nicolas <nicolasherman1103@gmail.com>
2025-11-29 19:23:23 +0100

    ended the display function

--- drawing_widget/main.js
+++ drawing_widget/main.js
@@ -120,7 +120,7 @@ dropArea.addEventListener('drop', async (e) => {
             features = format.readFeatures(geojson, { featureProjection: "EPSG:3857" });
         }
         else {
-            alert("Format non supporté !");
+            alert("Unsupported format !");
             return;
         }
         if (features.length > 0) {
@@ -131,8 +131,8 @@ dropArea.addEventListener('drop', async (e) => {
         }
 
     } catch (err) {
-        console.error("Erreur lors du chargement du fichier :", err);
-        alert("Erreur lors du chargement du fichier : " + err.message);
+        console.error("Error during file loading :", err);
+        alert("Error during file loading  : " + err.message);
     }
 });
 dropArea.addEventListener('dragover', (e) => {
@@ -158,7 +158,7 @@ fileInput.addEventListener("change", async (event) => {
     const fileName = file.name.toLowerCase();
     let features = [];
 
-    console.log("Fichier sélectionné :", fileName);
+    console.log("Selected file :", fileName);
 
     try {
 
@@ -192,12 +192,11 @@ fileInput.addEventListener("change", async (event) => {
             const extent = source.getExtent();
             map.getView().fit(extent, { padding: [20, 20, 20, 20] });
 
-            console.log("Nombre de features chargées :", features.length);
         }
 
     } catch (err) {
-        console.error("Erreur lors du chargement :", err);
-        alert("Erreur lors de la lecture du fichier.");
+        console.error("Error during file loading:", err);
+        alert("Erreur during file reading.");
     }
 });
 const kpPanel = document.getElementById("known-point-panel");
@@ -220,7 +219,7 @@ kpDrawBtn.addEventListener("click", () => {
     const radius = parseFloat(document.getElementById("kp_radius").value);
 
     if (isNaN(lon) || isNaN(lat) || isNaN(radius)) {
-        alert("Veuillez entrer longitude, latitude et rayon.");
+        alert("Please provide the three fields.");
         return;
     }
 
@@ -238,7 +237,6 @@ kpDrawBtn.addEventListener("click", () => {
     const extent = circle.getExtent();
     map.getView().fit(extent, { padding: [20, 20, 20, 20] });
 
-    console.log("Known point circle added");
 });
 map.getView().fit(ol.proj.get('EPSG:3857').getExtent(), { size: map.getSize() });
 let resultLayer = null;
@@ -248,37 +246,36 @@ document.getElementById('export').addEventListener('click', async () => {
     const features = source.getFeatures();
 
     if (features.length === 0) {
-        alert("Aucune géométrie sur la carte !");
+        alert("No geometries on the map !");
         return;
     }
+    // Unfortunatly, geojson don't support circle object, i have to transform it into point object 
+    // and add a radius property, backend retransform it into a polygone with shapely.buffer 
     const processedFeatures = [];
     features.forEach(f => {
         const geom = f.getGeometry();
 
         if (geom instanceof ol.geom.Circle) {
-            // Extraire centre + rayon
+            // Extract center+radius for added circle
             const center = geom.getCenter();
             const radius = geom.getRadius();
                         // Transformer le cercle → point GeoJSON
             const pointGeom = new ol.geom.Point(center);
 
-            // Créer une nouvelle feature à envoyer
+            // Create new feature for backend sending
             const newF = new ol.Feature({
                 geometry: pointGeom,
-                properties: {
-                    ...f.getProperties(),
-                    radius: radius,           // ← rayon ajouté
-                    original_type: "Circle"   // ← traçabilité
-                }
+                    radius: radius,           // ← radius
+                    original_type: "Circle"   // ← easier to read for debugging
             });
                         processedFeatures.push(newF);
 
         } else {
-            // Feature normale → ajout direct
+            // polygon feature extraction
             processedFeatures.push(f);
         }
     });
-    // Convertir en GeoJSON (objet JS)
+    // Convert to GeoJSON
     const geojsonObject = format.writeFeaturesObject(processedFeatures, {
         featureProjection: "EPSG:3857",
         dataProjection: "EPSG:3857"
@@ -298,28 +295,28 @@ document.getElementById('export').addEventListener('click', async () => {
         }
 
         const data = await response.json();
-        console.log("Réponse backend :", data);
+        console.log("backend response:", data);
 
         if (!data.data) {
-            console.error("GeoJSON manquant dans la réponse backend !");
+            console.error("No Geojson in backend response !");
             return;
         }
 
         const resultGeoJSON = data.data;
 
-        // Supprimer ancienne couche résultat
+        // deleting last querying (is thi good ?)
         if (resultLayer !== null) {
             map.removeLayer(resultLayer);
         }
 
-        // Construire la source vecteur
+        // Building the display with backend response
     const resultSource = new ol.source.Vector({ 
         features: new ol.format.GeoJSON().readFeatures(resultGeoJSON, { featureProjection: "EPSG:3857" }) });
     const resultStyle = (feature) => {
         const props = feature.getProperties();
-        const precision = props.precision || "";
+        const precision = props.level_of_accuracy || "";
 
-        // Liste des valeurs autorisées pour le style orange
+
         const orangeList = ["APPROXIMATE_LOCATION", "EXACT_LOCATION", "COORDINATES"];
 
         const isOrange = orangeList.includes(precision);
@@ -344,7 +341,7 @@ document.getElementById('export').addEventListener('click', async () => {
         });
     };
 
-        // Nouvelle couche
+        // New layer
         resultLayer = new ol.layer.Vector({
             source: resultSource,
             style: resultStyle
@@ -352,11 +349,11 @@ document.getElementById('export').addEventListener('click', async () => {
 
         map.addLayer(resultLayer);
 
-        // Zoom automatique sur les résultats
+        // Autoomatic zoom on results (don't think it's good)
         map.getView().fit(resultSource.getExtent(), { padding: [20,20,20,20], duration: 800 });
 
     } catch (err) {
         console.error(err);
-        alert("Erreur lors de l’envoi au backend");
+        alert("Error during sending to backend);
     }
 });
