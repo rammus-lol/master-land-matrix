8ab66be18a2f4115a7d500f02e97b65b5d968fe6
Arthur <arthur.univ@proton.me>
2026-02-27 14:21:13 +0100

    config for the prod vm

--- website/src/scripts/main.js
+++ website/src/scripts/main.js
@@ -1,4 +1,3 @@
-import  '../styles/maps.css';
 import Map from 'ol/Map';
 import View from 'ol/View.js';
 import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
--- website/vite.config.js
+++ website/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 5500,
-    open: 'firefox',
+    host: true,
+    allowedHosts: [
+      'landmatrix.artxypro.org'
+    ]
   }
-});
+})
