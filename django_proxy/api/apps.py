0531a4039a39415ebb36e9f7735e4119ebc78446
rammus-lol <nicolasherman1103@gmail.com>
2026-03-10 00:10:51 +0100

    Unlock memory optimization, now geopackages are loaded in ram at the beginning before any requests

--- django_proxy/api/apps.py
+++ django_proxy/api/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+    def ready(self):
+        from api.custom_service import spatial_function
+        print("--- Loading Geopackage in ram ---")
+        spatial_function.get_data()
+        print("--- Succes : Data ready in RAM ---")
