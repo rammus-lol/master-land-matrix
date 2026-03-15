10c43cf8ea3a47e88ad0774f8399abb806f734f9
rammus-lol <nicolasherman1103@gmail.com>
2026-03-15 19:33:25 +0100

    add os in import

--- django_proxy/proxy_project/settings.py
+++ django_proxy/proxy_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from environ import Env
 
@@ -92,7 +92,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
