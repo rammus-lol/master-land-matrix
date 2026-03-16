12b6149b785409cd258fc1065847994b68a3eb21
rammus-lol <nicolasherman1103@gmail.com>
2026-03-16 19:35:46 +0100

    add CeCILL licencing remove old code

--- django_proxy/manage.py
+++ django_proxy/manage.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+
+# # Copyright (c) 2026, Land Matrix Geodata Visualiser
+# All rights reserved.
+# 
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software.  You can  use, 
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
--- django_proxy/proxy_project/wsgi.py
+++ django_proxy/proxy_project/wsgi.py
@@ -1,3 +1,12 @@
+# # Copyright (c) 2026, Land Matrix Geodata Visualiser
+# All rights reserved.
+# 
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software.  You can  use, 
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+
 """
 WSGI config for proxy_project project.
 
